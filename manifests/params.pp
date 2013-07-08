5321f6f53f6e3ba475daf713f0b2cf668d7ab150
Tom De Vylder <tom@penumbra.be>
2013-07-08 14:37:56 +0200

    Bugfix: wrong pid file location

--- manifests/params.pp
+++ manifests/params.pp
@@ -28,7 +28,7 @@ class redis::params {
   $maxmemory_policy            = undef
   $maxmemory_samples           = undef
   $no_appendfsync_on_rewrite   = 'no'
-  $pid_file                    = '/var/run/redis/redis.pid'
+  $pid_file                    = '/var/run/redis/redis-server.pid'
   $port                        = '6379'
   $rdbcompression              = 'yes'
   $requirepass                 = undef
