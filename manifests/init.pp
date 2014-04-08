f5e784d2a8e78f6a95dad9c89c90582d8320c5ea
zebzeb <helle.sebastien@gmail.com>
2014-04-08 14:54:40 +0200

    change params  to

--- manifests/init.pp
+++ manifests/init.pp
@@ -47,7 +47,7 @@ class redis (
   $maxmemory_policy            = $::redis::params::maxmemory_policy,
   $maxmemory_samples           = $::redis::params::maxmemory_samples,
   $no_appendfsync_on_rewrite   = $::redis::params::no_appendfsync_on_rewrite,
-  $notifyservice               = $::redis::params::notifyservice,
+  $notify_service              = $::redis::params::notify_service,
   $package_ensure              = $::redis::params::package_ensure,
   $package_name                = $::redis::params::package_name,
   $pid_file                    = $::redis::params::pid_file,
@@ -82,7 +82,7 @@ class redis (
   include config
   include service
 
-  if $::redis::notifyservice == true {
+  if $::redis::notify_service == true {
     Class['preinstall'] ->
     Class['install'] ->
     Class['config'] ~>
