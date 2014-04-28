88fb9c65ee94c7cd0d02c9b7b387aec9a6d09bf1
jrnt30 <justin.r.nauman+github@gmail.com>
2014-04-28 11:58:29 -0500

    puppet-redis/issues/11 - Updating to support Amazon AMI explicitly

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -6,7 +6,7 @@
 class redis::preinstall {
   if $::redis::manage_repo {
     case $::operatingsystem {
-      'RedHat', 'CentOS', 'Scientific', 'OEL', 'Amazon': {
+      'RedHat', 'CentOS', 'Scientific', 'OEL': {
         $rpm_url = $::operatingsystemrelease ? {
           /^5/    => "http://download.powerstack.org/5/${::architecture}/",
           /^6/    => "http://download.powerstack.org/6/${::architecture}/",
@@ -16,6 +16,26 @@ class redis::preinstall {
         $rpm_gpgkey = $::operatingsystemrelease ? {
           /^5/    => 'https://raw.github.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
           /^6/    => 'https://raw.github.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
+          default => Fail['Operating system or release version not supported.'],
+        }
+
+        yumrepo { 'powerstack':
+          descr    => 'PowerStack for CentOS',
+          baseurl  => $rpm_url,
+          gpgkey   => $rpm_gpgkey,
+          enabled  => 1,
+          gpgcheck => 1;
+        }
+      }
+
+      'Amazon': {
+        $rpm_url = $::operatingsystemrelease ? {
+          /^3/    => "http://download.powerstack.org/6/${::architecture}/",
+          default => Fail['Operating system or release version not supported.'],
+        }
+
+        $rpm_gpgkey = $::operatingsystemrelease ? {
+          /^3/    => 'https://raw.github.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
           default => Fail['Operating system or release version not supported.'],
         }
 
