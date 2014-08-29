voxpupuli/puppet-redis

commit 6e1dd7f41485b8f18f11c4e3a06679d2f85a8d5d

Style fixes

manifests/init.pp
@@ -80,13 +80,7 @@ class redis (
   $zset_max_ziplist_entries    = $::redis::params::zset_max_ziplist_entries,
   $zset_max_ziplist_value      = $::redis::params::zset_max_ziplist_value,
 ) inherits redis::params {
-
-  include preinstall
-  include install
-  include config
-  include service
-
-  if $::redis::notify_service == true {
+  if $::redis::notify_service {
     Class['preinstall'] ->
     Class['install'] ->
     Class['config'] ~>

manifests/config.pp
@@ -3,8 +3,7 @@
 # This class provides configuration for Redis.
 #
 class redis::config {
-
-  if $::redis::notify_service == true {
+  if $::redis::notify_service {
     File {
       owner  => $::redis::config_owner,
       group  => $::redis::config_group,
@@ -13,9 +12,9 @@ class redis::config {
     }
   } else {
     File {
-      owner  => $::redis::config_owner,
-      group  => $::redis::config_group,
-      mode   => $::redis::config_file_mode,
+      owner => $::redis::config_owner,
+      group => $::redis::config_group,
+      mode  => $::redis::config_file_mode,
     }
   }
 

commit 6bd7766d68db67dff5faa0b1494bf7110ed69541

Fix bug caused by 6e1dd7f41485

manifests/init.pp
@@ -80,6 +80,11 @@ class redis (
   $zset_max_ziplist_entries    = $::redis::params::zset_max_ziplist_entries,
   $zset_max_ziplist_value      = $::redis::params::zset_max_ziplist_value,
 ) inherits redis::params {
+  include preinstall
+  include install
+  include config
+  include service
+
   if $::redis::notify_service {
     Class['preinstall'] ->
     Class['install'] ->