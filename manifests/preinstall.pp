312e002600e8b672543bee60c21e0173a072d3a5
Tom De Vylder <tom@penumbra.be>
2015-08-06 21:50:19 +0200

    Add EL7 repo management support

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -7,24 +7,30 @@ class redis::preinstall {
   if $::redis::manage_repo {
     case $::operatingsystem {
       'RedHat', 'CentOS', 'Scientific', 'OEL': {
-        $rpm_url = $::operatingsystemrelease ? {
-          /^5/    => "http://download.powerstack.org/5/${::architecture}/",
-          /^6/    => "http://download.powerstack.org/6/${::architecture}/",
-          default => Fail['Operating system or release version not supported.'],
+        if $::operatingsystemrelease < 7 {
+          $rpm_url = $::operatingsystemrelease ? {
+            /^5/    => "http://download.powerstack.org/5/${::architecture}/",
+            /^6/    => "http://download.powerstack.org/6/${::architecture}/",
+            default => Fail['Operating system or release version not supported.'],
+          }
+
+          $rpm_gpgkey = $::operatingsystemrelease ? {
+            /^5/    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
+            /^6/    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
+            default => Fail['Operating system or release version not supported.'],
+          }
+
+          yumrepo { 'powerstack':
+            descr    => 'PowerStack for CentOS',
+            baseurl  => $rpm_url,
+            gpgkey   => $rpm_gpgkey,
+            enabled  => 1,
+            gpgcheck => 1;
+          }
         }
 
-        $rpm_gpgkey = $::operatingsystemrelease ? {
-          /^5/    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
-          /^6/    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
-          default => Fail['Operating system or release version not supported.'],
-        }
-
-        yumrepo { 'powerstack':
-          descr    => 'PowerStack for CentOS',
-          baseurl  => $rpm_url,
-          gpgkey   => $rpm_gpgkey,
-          enabled  => 1,
-          gpgcheck => 1;
+        if $::operatingsystemrelease == 7 {
+          require ::epel
         }
       }
 
