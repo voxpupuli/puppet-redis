106d4632c5faeda853507eea6edd88df9e753031
Christoph Maser <christoph.maser@br.de>
2015-09-10 08:33:08 +0200

    use versioncmp() to check RedHat/CentOS/SCL version
    comparing CentOS version strings wich look like '7.1.1503' gives an error
    
    "Could not retrieve catalog from remote server: Error 400 on SERVER:
    comparison of String with 7 failed at modules/redis/manifests/preinstall.pp:10"

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -7,10 +7,10 @@ class redis::preinstall {
   if $::redis::manage_repo {
     case $::operatingsystem {
       'RedHat', 'CentOS', 'Scientific', 'OEL': {
-        if $::operatingsystemmajrelease < '7' {
-          $rpm_url = $::operatingsystemmajrelease ? {
-            '5'    => "http://download.powerstack.org/5/${::architecture}/",
-            '6'    => "http://download.powerstack.org/6/${::architecture}/",
+        if (versioncmp($::operatingsystemrelease, '7.0') == -1) {
+          $rpm_url = $::operatingsystemrelease ? {
+            /^5/    => "http://download.powerstack.org/5/${::architecture}/",
+            /^6/    => "http://download.powerstack.org/6/${::architecture}/",
             default => Fail['Operating system or release not supported.'],
           }
 
@@ -29,7 +29,7 @@ class redis::preinstall {
           }
         }
 
-        if $::operatingsystemmajrelease == '7' {
+        if (versioncmp($::operatingsystemrelease, '7.0') >= 0) {
           require ::epel
         }
       }
