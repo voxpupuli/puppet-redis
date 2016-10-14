88d8376138290f8d879e58a80d5e19232b6f34b3
Peter Souter <peter.souter+GIT@puppet.com>
2016-10-14 22:53:40 +0100

    Fixes Debian apt source spec

--- spec/classes/redis_spec.rb
+++ spec/classes/redis_spec.rb
@@ -329,8 +329,16 @@ describe 'redis', :type => :class do
         }
       }
 
-      it { should create_apt__key('dotdeb') }
-      it { should create_apt__source('dotdeb') }
+      it { should create_apt__source('dotdeb').with(
+        :location => 'http://packages.dotdeb.org/',
+          :release  =>  'wheezy',
+          :repos    => 'all',
+          :key      => {
+            "id"=>"6572BBEF1B5FF28B28B706837E3F070089DF5277",
+            "source"=>"http://www.dotdeb.org/dotdeb.gpg"
+          },
+          :include  => { 'src' => true },
+      ) }
     end
 
     context 'on Ubuntu' do
