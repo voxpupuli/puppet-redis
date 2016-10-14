ed26d698ee901f964f74d52d33a1d2b3129398ce
Peter Souter <peter.souter+GIT@puppet.com>
2016-10-14 22:57:34 +0100

    Move Repo for RHEL systems to EPEL
    * powerstack.org no longer exists
    * Redis is now in EPEL
    
    Closes #103

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -6,52 +6,8 @@
 class redis::preinstall {
   if $::redis::manage_repo {
     case $::operatingsystem {
-      'RedHat', 'CentOS', 'Scientific', 'OEL': {
-        if (versioncmp($::operatingsystemrelease, '7.0') == -1) {
-          $rpm_url = $::operatingsystemrelease ? {
-            /^5/    => "http://download.powerstack.org/5/${::architecture}/",
-            /^6/    => "http://download.powerstack.org/6/${::architecture}/",
-            default => Fail['Operating system or release not supported.'],
-          }
-
-          $rpm_gpgkey = $::operatingsystemmajrelease ? {
-            '5'    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
-            '6'    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
-            default => Fail['Operating system or release not supported.'],
-          }
-
-          yumrepo { 'powerstack':
-            descr    => 'PowerStack for CentOS',
-            baseurl  => $rpm_url,
-            gpgkey   => $rpm_gpgkey,
-            enabled  => 1,
-            gpgcheck => 1;
-          }
-        }
-
-        if (versioncmp($::operatingsystemmajrelease, '7') >= 0) {
-          require ::epel
-        }
-      }
-
-      'Amazon': {
-        $rpm_url = $::operatingsystemmajrelease ? {
-          '3'    => "http://download.powerstack.org/6/${::architecture}/",
-          default => Fail['Operating system or release version not supported.'],
-        }
-
-        $rpm_gpgkey = $::operatingsystemmajrelease ? {
-          '3'    => 'https://raw.githubusercontent.com/santisaez/powerstack/master/RPM-GPG-KEY-powerstack',
-          default => Fail['Operating system or release version not supported.'],
-        }
-
-        yumrepo { 'powerstack':
-          descr    => 'PowerStack for CentOS',
-          baseurl  => $rpm_url,
-          gpgkey   => $rpm_gpgkey,
-          enabled  => 1,
-          gpgcheck => 1;
-        }
+      'RedHat', 'CentOS', 'Scientific', 'OEL', 'Amazon': {
+        require ::epel
       }
 
       'Debian': {
