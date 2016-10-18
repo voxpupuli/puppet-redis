8e1b116cb95d57c5f0f4dd939b47fa4d9666f304
Pankaj Kaushal <pankaj.kaushal@booking.com>
2016-10-18 14:26:38 +0200

    Making slaveof_port empty would make it backward compatible, but quite confusing. this being undef is better, but putting it empty so that existing conf does not break.

--- manifests/params.pp
+++ manifests/params.pp
@@ -98,7 +98,7 @@ class redis::params {
   $slave_read_only          = true
   $slave_serve_stale_data   = true
   $slaveof                  = undef
-  $slaveport                = undef
+  $slaveport                = ''
 
   # redis.conf.erb - redis 3.0 clustering
   $cluster_enabled        = false
