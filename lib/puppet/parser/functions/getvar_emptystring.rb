32fbf433da7da933b4e3b8d3600f4b55c5198d3e
Peter Souter <peter.souter+GIT@puppet.com>
2017-05-16 09:32:30 +0100

    Updates docs for puppet functions
    * redisget() doc update
    * getvar_emptystring() doc update

--- lib/puppet/parser/functions/getvar_emptystring.rb
+++ lib/puppet/parser/functions/getvar_emptystring.rb
@@ -1,20 +1,24 @@
 module Puppet::Parser::Functions
 
-  newfunction(:getvar_emptystring, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
-    Lookup a variable in a remote namespace.
-    Return an empty string rather than undef/nil if it could not be found
-
-    For example:
-
-        $foo = getvar('site::data::foo')
-        # Equivalent to $foo = $site::data::foo
-
-    This is useful if the namespace itself is stored in a string:
-
-        $datalocation = 'site::data'
-        $bar = getvar("${datalocation}::bar")
-        # Equivalent to $bar = $site::data::bar
-    ENDHEREDOC
+  newfunction(:getvar_emptystring, :type => :rvalue, :doc => <<-DOC
+Return a variable in a remote namespace, but returns an empty
+string if the variable is undefined or can't be found.
+This is useful when trying to lookup a value that might be undefined
+on first run, like a fact for an application that will be installed as
+part of a Puppet run, but you don't want the reference to an undefined variable
+to cause an error when strict variables are enabled.
+@param variable [String] The variable to lookup
+@return [String] The value of the variable if found
+@return [String] An empty string if the variable is not found eg. `''`
+@example Checking a variable exists (Equivalent to $foo = $site::data::foo)
+  $foo = getvar('site::data::foo')
+@example Calling the function. (Equivalent to $bar = $site::data::bar)
+  $datalocation = 'site::data'
+  $bar = getvar("${datalocation}::bar")
+@example Combining with the pick() function
+  $redis_version_real = pick(getvar_emptystring('redis_server_version'), '3.2.1')
+DOC
+  ) do |args|
 
     unless args.length == 1
       raise Puppet::ParseError, ("getvar_emptystring(): wrong number of arguments (#{args.length}; must be 1)")
