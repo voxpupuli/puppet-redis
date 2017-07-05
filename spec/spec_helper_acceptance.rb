65b6b8bfb9449247eb66a23840c86b6b12228ae6
Peter Souter <peter.souter+GIT@puppet.com>
2017-07-05 14:00:55 +0100

    Update EPEL module
    * Fixes endpoints to be https
    * Fixed a long time ago (https://github.com/stahnma/puppet-module-epel/issues/34)
    * Updates spec_helper_acceptance and metadata

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -22,7 +22,7 @@ RSpec.configure do |c|
       end
       on host, puppet('module', 'install', 'puppetlabs-stdlib -v 4.11.0'), { :acceptable_exit_codes => [0] }
       on host, puppet('module', 'install', 'puppetlabs-apt -v 2.3.0'), { :acceptable_exit_codes => [0] }
-      on host, puppet('module', 'install', 'stahnma-epel -v 1.0.2'), { :acceptable_exit_codes => [0] }
+      on host, puppet('module', 'install', 'stahnma-epel -v 1.2.2'), { :acceptable_exit_codes => [0] }
       on host, puppet('module', 'install', 'herculesteam/augeasproviders_core -v 2.1.0'), { :acceptable_exit_codes => [0] }
       on host, puppet('module', 'install', 'herculesteam/augeasproviders_sysctl -v 2.1.0'), { :acceptable_exit_codes => [0] }
     end
