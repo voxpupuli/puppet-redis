1ac33af61a2d788cbbbead4b128f89a393f2fdcb
Alex Harvey <alexharv074@gmail.com>
2019-03-04 15:17:12 +1100

    Corrections for Ubuntu in tests
    The tests appear to have not been updated for the correct file mode on
    Ubuntu.

--- spec/classes/redis_spec.rb
+++ spec/classes/redis_spec.rb
@@ -39,29 +39,29 @@ describe 'redis', :type => :class do
         case facts[:operatingsystem]
           when 'Debian'
 
-          context 'on Debian' do
+            context 'on Debian' do
+
+              it do
+                is_expected.to contain_file('/var/run/redis').with({
+                  :ensure => 'directory',
+                  :owner  => 'redis',
+                  :group  => 'root',
+                  :mode   => '2775',
+                })
+              end
+
+            end
+
+          when 'Ubuntu'
 
             it do
               is_expected.to contain_file('/var/run/redis').with({
                 :ensure => 'directory',
                 :owner  => 'redis',
-                :group  => 'root',
+                :group  => 'redis',
                 :mode   => '2775',
               })
             end
-
-          end
-
-        when 'Ubuntu'
-
-          it do
-            is_expected.to contain_file('/var/run/redis').with({
-              :ensure => 'directory',
-              :owner  => 'redis',
-              :group  => 'redis',
-              :mode   => '0755',
-            })
-          end
 
         end
       end
