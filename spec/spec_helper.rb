18cfd1c087a693e9e05f67e2fed190a54fc3bf41
Alexander Fisher <alex@linfratech.co.uk>
2019-03-08 14:57:15 +0000

    Rubocop Autofix: Lint/DeprecatedClassMethods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ end
 def get_spec_fixtures_dir
   spec_dir = File.expand_path(File.dirname(__FILE__) + '/fixtures')
 
-  raise "The directory #{spec_dir} does not exist" unless Dir.exists? spec_dir
+  raise "The directory #{spec_dir} does not exist" unless Dir.exist? spec_dir
 
   spec_dir
 end
@@ -41,7 +41,7 @@ end
 def read_fixture_file(filename)
   filename = get_spec_fixtures_dir + "/#{filename}"
 
-  raise "The fixture file #{filename} doesn't exist" unless File.exists? filename
+  raise "The fixture file #{filename} doesn't exist" unless File.exist? filename
 
   File.read(filename)
 end
