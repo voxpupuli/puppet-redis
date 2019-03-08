d043506062be8efe545a7b47903b91dd2de6d624
Alexander Fisher <alex@linfratech.co.uk>
2019-03-08 14:45:22 +0000

    Rubocop Autofix: Style/NumericLiterals

--- spec/classes/redis_sentinel_spec.rb
+++ spec/classes/redis_sentinel_spec.rb
@@ -73,7 +73,7 @@ describe 'redis::sentinel', type: :class do
         master_name: 'cow',
         down_after: 6000,
         log_file: '/tmp/barn-sentinel.log',
-        failover_timeout: 28000,
+        failover_timeout: 28_000,
         notification_script: 'bar.sh',
         client_reconfig_script: 'foo.sh'
       }
