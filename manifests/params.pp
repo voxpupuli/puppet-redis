2553486af981ea1c947e96ba538ef0b761457458
gery6 <gery@net6.at>
2019-12-05 08:58:41 +0100

    Debian10 fix
    Fix $sentinel_pid_file  for Debian.
    Systemd will lookup for the pidfile in /var/run/sentinel/redis-sentinel.pid

--- manifests/params.pp
+++ manifests/params.pp
@@ -23,7 +23,7 @@ class redis::params inherits redis::globals {
       $sentinel_daemonize        = true
       $sentinel_init_script      = '/etc/init.d/redis-sentinel'
       $sentinel_package_name     = 'redis-sentinel'
-      $sentinel_pid_file         = '/var/run/redis/redis-sentinel.pid'
+      $sentinel_pid_file         = '/var/run/sentinel/redis-sentinel.pid'
       $sentinel_log_file         = '/var/log/redis/redis-sentinel.log'
       $sentinel_working_dir      = '/var/lib/redis'
 
