d2bdbf57d601519f7a61ad83745ab8586ff889bd
Evgeny Liskovets <eugene.liskovets@gmail.com>
2020-08-03 09:47:07 +0200

    Sentinel's protected mode param is boolean.

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,7 +25,7 @@ class redis::params inherits redis::globals {
       $sentinel_package_name     = 'redis-sentinel'
       $sentinel_log_file         = '/var/log/redis/redis-sentinel.log'
       $sentinel_working_dir      = '/var/lib/redis'
-      $sentinel_protected_mode   = 'yes'
+      $sentinel_protected_mode   = true
 
       case $facts['os']['name'] {
         'Ubuntu': {
@@ -64,7 +64,7 @@ class redis::params inherits redis::globals {
       $sentinel_daemonize   = false
       $sentinel_init_script = undef
       $sentinel_working_dir = '/tmp'
-      $sentinel_protected_mode   = 'yes'
+      $sentinel_protected_mode   = true
 
       $scl = $redis::globals::scl
       if $scl {
@@ -136,7 +136,7 @@ class redis::params inherits redis::globals {
       $sentinel_pid_file         = '/var/run/redis/redis-sentinel.pid'
       $sentinel_log_file         = '/var/log/redis/sentinel.log'
       $sentinel_working_dir      = '/tmp'
-      $sentinel_protected_mode   = 'yes'
+      $sentinel_protected_mode   = true
 
       # pkg version
       $minimum_version           = '3.2.4'
@@ -166,7 +166,7 @@ class redis::params inherits redis::globals {
       $sentinel_pid_file         = '/var/run/redis/redis-sentinel.pid'
       $sentinel_log_file         = '/var/log/redis/sentinel.log'
       $sentinel_working_dir      = '/tmp'
-      $sentinel_protected_mode   = 'yes'
+      $sentinel_protected_mode   = true
 
       # suse package version
       $minimum_version           = '3.0.5'
@@ -197,7 +197,7 @@ class redis::params inherits redis::globals {
       $sentinel_pid_file         = '/var/run/redis/redis-sentinel.pid'
       $sentinel_log_file         = '/var/log/redis/sentinel.log'
       $sentinel_working_dir      = '/tmp'
-      $sentinel_protected_mode   = 'yes'
+      $sentinel_protected_mode   = true
 
       # pkg version
       $minimum_version           = '3.2.4'
