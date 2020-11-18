065085e7308437f6de1591b316020e4e7e0bfb10
Sebastian Treu <s.treu@neusta.de>
2020-11-18 16:46:51 +0100

    add sentinel multi monitor

--- spec/acceptance/suites/default/redis_sentinel_one_node_spec.rb
+++ spec/acceptance/suites/default/redis_sentinel_one_node_spec.rb
@@ -11,9 +11,16 @@ describe 'redis::sentinel' do
   it 'runs successfully' do
     pp = <<-EOS
     class { 'redis::sentinel':
-      master_name      => 'mymaster',
-      redis_host       => '127.0.0.1',
-      failover_timeout => 10000,
+      sentinel_monitor => {
+        'mymaster' => {
+          redis_host             => '127.0.0.1',
+          redis_port             => 6379,
+          quorum                 => 2,
+          parallel_sync          => 1,
+          down_after             => 30000,
+          failover_timeout       => 10000,
+        },
+      },
     }
     EOS
 
--- spec/classes/redis_sentinel_spec.rb
+++ spec/classes/redis_sentinel_spec.rb
@@ -77,16 +77,23 @@ CONFIG
       describe 'with custom parameters' do
         let(:params) do
           {
-            auth_pass: 'password',
+            sentinel_monitor: {
+              'mymaster' => {
+                'redis_host'             => '127.0.0.1',
+                'redis_port'             => 6379,
+                'quorum'                 => 2,
+                'parallel_sync'          => 1,
+                'auth_pass'              => 'password',
+                'down_after'             => 6000,
+                'failover_timeout'       => 28_000,
+                'notification_script'    => '/path/to/bar.sh',
+                'client_reconfig_script' => '/path/to/foo.sh',
+              },
+            },
             sentinel_bind: '192.0.2.10',
             protected_mode: false,
-            master_name: 'cow',
-            down_after: 6000,
             working_dir: '/tmp/redis',
-            log_file: '/tmp/barn-sentinel.log',
-            failover_timeout: 28_000,
-            notification_script: '/path/to/bar.sh',
-            client_reconfig_script: '/path/to/foo.sh'
+            log_file: '/tmp/barn-sentinel.log'
           }
         end
 
@@ -98,13 +105,13 @@ dir /tmp/redis
 daemonize #{facts[:osfamily] == 'RedHat' ? 'no' : 'yes'}
 pidfile #{pidfile}
 #{protected_mode ? "protected-mode no\n" : ''}
-sentinel monitor cow 127.0.0.1 6379 2
-sentinel down-after-milliseconds cow 6000
-sentinel parallel-syncs cow 1
-sentinel failover-timeout cow 28000
-sentinel auth-pass cow password
-sentinel notification-script cow /path/to/bar.sh
-sentinel client-reconfig-script cow /path/to/foo.sh
+sentinel monitor mymaster 127.0.0.1 6379 2
+sentinel down-after-milliseconds mymaster 6000
+sentinel parallel-syncs mymaster 1
+sentinel failover-timeout mymaster 28000
+sentinel auth-pass mymaster password
+sentinel notification-script mymaster /path/to/bar.sh
+sentinel client-reconfig-script mymaster /path/to/foo.sh
 
 loglevel notice
 logfile /tmp/barn-sentinel.log
@@ -119,15 +126,22 @@ CONFIG
       describe 'with array sentinel bind' do
         let(:params) do
           {
-            auth_pass: 'password',
+            sentinel_monitor: {
+              'cow' => {
+                'redis_host'             => '127.0.0.1',
+                'redis_port'             => 6379,
+                'quorum'                 => 2,
+                'parallel_sync'          => 1,
+                'auth_pass'              => 'password',
+                'down_after'             => 6000,
+                'failover_timeout'       => 28_000,
+                'notification_script'    => '/path/to/bar.sh',
+                'client_reconfig_script' => '/path/to/foo.sh',
+              },
+            },
             sentinel_bind: ['192.0.2.10', '192.168.1.1'],
-            master_name: 'cow',
-            down_after: 6000,
             working_dir: '/tmp/redis',
-            log_file: '/tmp/barn-sentinel.log',
-            failover_timeout: 28_000,
-            notification_script: '/path/to/bar.sh',
-            client_reconfig_script: '/path/to/foo.sh'
+            log_file: '/tmp/barn-sentinel.log'
           }
         end
 
