44bdf3c682c62c7a3bcfd8e26586af12f822df4a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-02-07 13:08:46 +0100

    Only install EPEL on EL7
    In EL8 (and EL9) Redis is included in the OS itself. Only on EL7 it
    needs to be installed from EPEL. Even then, it can be installed via
    Software Collections which are included in the OS.

--- spec/classes/redis_spec.rb
+++ spec/classes/redis_spec.rb
@@ -61,11 +61,15 @@ describe 'redis' do
               with_service_name('rh-redis5-redis')
           end
 
-          context 'manage_repo => true', if: facts[:operatingsystem] == 'CentOS' do
+          context 'manage_repo => true' do
             let(:params) { { manage_repo: true } }
 
             it { is_expected.to compile.with_all_deps }
-            it { is_expected.to contain_package('centos-release-scl-rh') }
+            if facts[:operatingsystem] == 'CentOS'
+              it { is_expected.to contain_package('centos-release-scl-rh') }
+            else
+              it { is_expected.not_to contain_package('centos-release-scl-rh') }
+            end
           end
         end
       end
@@ -466,7 +470,7 @@ describe 'redis' do
       describe 'with parameter: manage_repo' do
         let(:params) { { manage_repo: true } }
 
-        if facts[:osfamily] == 'RedHat'
+        if facts[:osfamily] == 'RedHat' && facts[:operatingsystemmajrelease].to_i <= 7
           it { is_expected.to contain_class('epel') }
         else
           it { is_expected.not_to contain_class('epel') }
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,7 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  if fact_on(host, 'osfamily') == 'RedHat'
+  if fact_on(host, 'osfamily') == 'RedHat' && fact_on(host, 'operatingsystemmajrelease') == '7'
     install_module_from_forge_on(host, 'puppet/epel', '>= 3.0.0')
   end
   host.install_package('puppet-bolt')
