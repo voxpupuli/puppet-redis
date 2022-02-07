754a51e4d33a689ed40b4b976a4d6fd6c990c3d2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-02-07 18:45:22 +0100

    Install sysctl in non-docker
    This allows the tests to pass in Vagrant where the administration tests
    do run.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,6 +1,11 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
+  # sysctl is untestable in docker
+  unless host['hypervisor'] == 'docker'
+    install_module_from_forge_on(host, 'herculesteam/augeasproviders_sysctl', '>= 2.1.0 < 3.0.0')
+  end
+
   if fact_on(host, 'osfamily') == 'RedHat' && fact_on(host, 'operatingsystemmajrelease').to_i == 7
     install_module_from_forge_on(host, 'puppet/epel', '>= 3.0.0')
   end
