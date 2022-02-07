710b84b5e1b9bee4b645dc840e4b7b1ae6737ee7
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-02-07 12:48:31 +0100

    Set manage_repo to true in acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,8 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  if fact_on(host, 'operatingsystem') == 'CentOS'
-    host.install_package('epel-release')
+  if fact_on(host, 'osfamily') == 'RedHat'
+    install_module_from_forge_on(host, 'puppet/epel', '>= 3.0.0')
   end
   host.install_package('puppet-bolt')
 end
